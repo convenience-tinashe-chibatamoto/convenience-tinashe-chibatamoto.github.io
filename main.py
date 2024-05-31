a46fee8846721eedb8df0adcd2cf46da4ff534ab
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-31 09:00:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Convenience Tinashe Chibatamoto")
+    content = "Hi, I am Convenience! I am a Python programmer, author, and AI enthusiast.
+    st.info(content)
+
+content2 = "Below you can find some of the apps I have built in Python. Feel free to contact me."
+st.write(content2)
