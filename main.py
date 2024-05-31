9746b74da6113af58cadf06c05ad71c6dde0a270
Convenience  Chibatamoto <conveniencechibatamoto@gmail.com>
2024-05-31 11:51:55 +0200

    Visual Improvements

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/photo.png")
+    content2 = """
+As I continue on my journey as a software developer, I'm constantly amazed by the new possibilities that emerge. Whether I'm automating tedious tasks, building data-driven web applications, or exploring the frontiers of artificial intelligence, Python is usually the first door I knock to help me bring my ideas to life. If you're also a Python and data science enthusiast, I'd love to connect and discuss our shared passion in this space. Who knows, maybe we can even collaborate on a project or two down the line! 
+    
+Below you can find some of the apps I have built in Python. Feel free to reach out anytime:
+    
+- Email: conveniencechibatamoto@gmail.com
+- LinkedIn: https://www.linkedin.com/in/convenience-tinashe-chibatamoto
+- Github: https://github.com/convenience-tinashe-chibatamoto
+- Kaggle: https://www.kaggle.com/linedpenguin
+"""
+st.info(content2)
 
 with col2:
     st.title("Convenience Tinashe Chibatamoto")
@@ -18,18 +29,6 @@ I've been working with Python for the past 3 years, and I've grown to absolutely
 Over the years, I've had the opportunity to apply Python in a wide range of domains, from web development and data analysis to machine learning and automation. The rich ecosystem of libraries and frameworks, such as Django, NumPy, and TensorFlow, has been instrumental in helping me tackle complex problems and build robust, scalable applications. 
     """
     st.info(content)
-
-content2 = """
-As I continue on my journey as a software developer, I'm constantly amazed by the new possibilities that emerge. Whether I'm automating tedious tasks, building data-driven web applications, or exploring the frontiers of artificial intelligence, Python is usually the first door I knock to help me bring my ideas to life. If you're also a Python and data science enthusiast, I'd love to connect and discuss our shared passion in this space. Who knows, maybe we can even collaborate on a project or two down the line! 
-    
-Below you can find some of the apps I have built in Python. Feel free to reach out anytime:
-    
-- Email: conveniencechibatamoto@gmail.com
-- LinkedIn: https://www.linkedin.com/in/convenience-tinashe-chibatamoto
-- Github: https://github.com/convenience-tinashe-chibatamoto
-- Kaggle: https://www.kaggle.com/linedpenguin
-"""
-st.write(content2)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
